f329f456d44533d1f5dfe1bb68c3abc9cb349048
BKhenloo <74878006+BKhenloo@users.noreply.github.com>
2020-12-29 22:05:05 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,15 @@
-#! /usr/bin/env bash
-
-## Create openhab user
-echo $(adduser --no-create-home --user-group --uid $UID --gid $GID openhab)
-
-## Copy openHAB content to working dir, if empty
-if [ -z "$(ls -A /opt/openhab)" ]; then
-  echo "Copy openHAB content to working directory..."
-  tar xafv /tmp/openhab.tar.gz
-else
-  echo "Found existing openHAB..."
-fi
-
-## Set ownership of working dir
-chown openhab:openhab -R /opt/openhab
-
-## Start openHAB instance 
-sudo -u openhab /usr/bin/env bash -c '/opt/openhab/start.sh'
+#! /usr/bin/env bash                                                            
+                                                                                
+## Copy openHAB content to working dir, if empty                                
+if [ -z "$(ls -A /opt/openhab)" ]; then                                         
+  echo "Copy openHAB content to working directory..."                           
+  tar xafv /tmp/openhab.tar.gz -C /opt/openhab                                  
+else                                                                            
+  echo "Use existing data at openHAB working dir..."                            
+fi                                                                              
+                                                                                
+##                                                                              
+echo "Try to run: $@ "                                                          
+                                                                                
+## Start openHAB instance                                                       
+exec $@
